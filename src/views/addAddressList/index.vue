bc6dfd4cbf6df5a0f1f9fb0b3f80e7c8d71e1de2
yiwanghushanxing <752229596@qq.com>
2020-05-29 11:19:04 +0800

    aa

--- src/views/addAddressList/index.vue
+++ src/views/addAddressList/index.vue
@@ -11,21 +11,22 @@
 "
     />
     <van-address-edit
-        :area-list="areaList"
-        show-postal
-        show-delete
-        show-set-default
-        show-search-result
-        :search-result="searchResult"
-        :area-columns-placeholder="['请选择', '请选择', '请选择']"
-        @save="onSave"
-        @delete="onDelete"
-        @change-detail="onChangeDetail"
-    />
+  :area-list="areaList"
+  show-postal
+  show-delete
+  show-set-default
+  show-search-result
+  :search-result="searchResult"
+  :area-columns-placeholder="['请选择', '请选择', '请选择']"
+  @save="onSave"
+  @delete="onDelete"
+  @change-detail="onChangeDetail"
+/>
 </div>
 </template>
 
 <script>
+import { Area } from 'vant';
 import { Toast } from 'vant';
 export default {
 data() {
